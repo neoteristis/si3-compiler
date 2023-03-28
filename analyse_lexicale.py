acfb90eaa7db6f0a94494fb64cc6ad5d65744836
Tom Bedino <tom.bedino@etu.univ-cotedazur.fr>
2023-03-28 21:54:55 +0200

    feat: Add Lexer rules

--- analyse_lexicale.py
+++ analyse_lexicale.py
@@ -3,22 +3,39 @@ from sly import Lexer
 
 class FloLexer(Lexer):
 	# Noms des lexèmes (sauf les litéraux). En majuscule. Ordre non important
-	tokens = { IDENTIFIANT, ENTIER, ECRIRE,INFERIEUR_OU_EGAL }
+	tokens = { IDENTIFIANT, ENTIER, BOOLEAN, ECRIRE, INFERIEUR_OU_EGAL,SUPERIEUR_OU_EGAL,
+			   DIFFERENT, EQUAL, ET, OU, NON, SI, SINON, TANTQUE, RETOURNER , AFFECT, SEPARATOR}
 
 	#Les caractères litéraux sont des caractères uniques qui sont retournés tel quel quand rencontré par l'analyse lexicale. 
 	#Les litéraux sont vérifiés en dernier, après toutes les autres règles définies par des expressions régulières.
 	#Donc, si une règle commence par un de ces littérals (comme INFERIEUR_OU_EGAL), cette règle aura la priorité.
-	literals = { '+','*','(',')',";" }
+	literals = {'+', '-' , '*', '(', ')', ';', "{", "}", "%", "/", '<', '>'}
 	
 	# chaines contenant les caractère à ignorer. Ici espace et tabulation
 	ignore = ' \t'
 
 	# Expressions régulières correspondant au différents Lexèmes par ordre de priorité
 	INFERIEUR_OU_EGAL= r'<='
+	SUPERIEUR_OU_EGAL= r'>='
+	DIFFERENT = r'!='
+	EQUAL = r'=='
+
+	AFFECT = r'='
+	SEPARATOR = r','
+
+	# opérateur booléen
+	ET = r'et'
+	OU = r'ou'
+	NON = r'non'
 	
 	@_(r'0|[1-9][0-9]*')
 	def ENTIER(self, t):
 		t.value = int(t.value)
+		return t
+
+	@_(r'Vrai|Faux')
+	def BOOLEAN(self, t):
+		t.value = bool(t.value)
 		return t
 
     	# cas général
@@ -26,6 +43,10 @@ class FloLexer(Lexer):
 
 	# cas spéciaux:
 	IDENTIFIANT['ecrire'] = ECRIRE
+	IDENTIFIANT['si'] = SI
+	IDENTIFIANT['sinon'] = SINON
+	IDENTIFIANT['tantque'] = TANTQUE
+	IDENTIFIANT['retourner'] = RETOURNER
 	
 	#Syntaxe des commentaires à ignorer
 	ignore_comment = r'\#.*'
